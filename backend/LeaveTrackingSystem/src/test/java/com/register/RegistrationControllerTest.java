4ec2da4c81ccd239861745afbd5f809f1126fa14
NamalaAnu04 <83859305+NamalaAnu04@users.noreply.github.com>
2023-10-03 12:19:00 +0530

    Update RegistrationControllerTest.java

--- backend/LeaveTrackingSystem/src/test/java/com/register/RegistrationControllerTest.java
+++ backend/LeaveTrackingSystem/src/test/java/com/register/RegistrationControllerTest.java
@@ -29,66 +29,37 @@ public class RegistrationControllerTest {
 
     @Test
     public void testRegisterUser_Success() throws Exception {
-        // Create a sample user
         User user = new User();
-        user.setEmail("test@example.com");
-
-        // Mock the service method to return null (user does not exist)
-        when(RegistrationService.fetchUserByEmail("test@example.com")).thenReturn(null);
-
-        // Mock the service method to save the user
+        user.setEmail("anunamala04@gmail.com");
+        when(RegistrationService.fetchUserByEmail("anunamala04@gmail.com")).thenReturn(null);
         when(RegistrationService.saveUser(user)).thenReturn(user);
-
-        // Call the controller method
         User registeredUser = registrationController.registerUser(user);
-
-        // Verify that the service methods were called
-        verify(RegistrationService, times(1)).fetchUserByEmail("test@example.com");
+        verify(RegistrationService, times(1)).fetchUserByEmail("anunamala04@gmail.com");
         verify(RegistrationService, times(1)).saveUser(user);
-
-        // Assert the result
         assertNotNull(registeredUser);
-        assertEquals("test@example.com", registeredUser.getEmail());
+        assertEquals("anunamala04@gmail.com"), registeredUser.getEmail());
     }
 
     @Test
     public void testRegisterUser_UserExists() {
-        // Create a sample user
         User user = new User();
-        user.setEmail("test@example.com");
-
-        // Mock the service method to return an existing user
-        when(RegistrationService.fetchUserByEmail("test@example.com")).thenReturn(user);
-
-        // Call the controller method and expect an exception
+        user.setEmail("anunamala04@gmail.com");
+        when(RegistrationService.fetchUserByEmail("anunamala04@gmail.com")).thenReturn(user);
         Exception exception = assertThrows(Exception.class, () -> {
         	registrationController.registerUser(user);
         });
-
-        // Verify that the service method was called
-        verify(RegistrationService, times(1)).fetchUserByEmail("test@example.com");
-
-        // Assert the exception message
+        verify(RegistrationService, times(1)).fetchUserByEmail("anunamala04@gmail.com");
         assertEquals("User with test@example.com is already Exists", exception.getMessage());
     }
     
     @Test
     public void testLoginUser_Success() throws Exception {
-        // Create a sample user
         User user = new User();
         user.setEmail("test@example.com");
         user.setPassword("password123");
-
-        // Mock the service method to return a valid user
         when(RegistrationService.fetchUserByEmailAndPassword("test@example.com", "password123")).thenReturn(user);
-
-        // Call the controller method
         User loggedInUser = registrationController.loginUser(user);
-
-        // Verify that the service method was called
         verify(RegistrationService, times(1)).fetchUserByEmailAndPassword("test@example.com", "password123");
-
-        // Assert the result
         assertNotNull(loggedInUser);
         assertEquals("test@example.com", loggedInUser.getEmail());
         assertEquals("password123", loggedInUser.getPassword());
@@ -96,23 +67,14 @@ public class RegistrationControllerTest {
 
     @Test
     public void testLoginUser_InvalidCredentials() {
-        // Create a sample user
         User user = new User();
         user.setEmail("test@example.com");
         user.setPassword("password123");
-
-        // Mock the service method to return null (invalid credentials)
         when(RegistrationService.fetchUserByEmailAndPassword("test@example.com", "password123")).thenReturn(null);
-
-        // Call the controller method and expect an exception
         Exception exception = assertThrows(Exception.class, () -> {
             registrationController.loginUser(user);
         });
-
-        // Verify that the service method was called
         verify(RegistrationService, times(1)).fetchUserByEmailAndPassword("test@example.com", "password123");
-
-        // Assert the exception message
         assertEquals("Invalid Credentials", exception.getMessage());
     }
 }
