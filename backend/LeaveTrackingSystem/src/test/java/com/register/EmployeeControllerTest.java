f263f7bdb9bbbcb89a34d8ffe23710ec03b2bfa1
NamalaAnu04 <83859305+NamalaAnu04@users.noreply.github.com>
2023-10-03 12:15:06 +0530

    Update EmployeeControllerTest.java

--- backend/LeaveTrackingSystem/src/test/java/com/register/EmployeeControllerTest.java
+++ backend/LeaveTrackingSystem/src/test/java/com/register/EmployeeControllerTest.java
@@ -35,61 +35,31 @@ public class EmployeeControllerTest {
 
     @Test
     public void testGetLeaveCount_Success() {
-        // Define a sample email
-        String email = "user@example.com";
-
-        // Mock the service method to return a leave count
-        when(leaveService.getLeaveCountByEmail(email)).thenReturn(5); // Replace 5 with the expected count
-
-        // Call the controller method
+        String email = "anunamala04@gmail.com";
+        when(leaveService.getLeaveCountByEmail(email)).thenReturn(9);
         ResponseEntity<Integer> response = leaveController.getLeaveCount(email);
-
-        // Verify that the service method was called
         verify(leaveService, times(1)).getLeaveCountByEmail(email);
-
-        // Assert the response status code and body
         assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals(5, response.getBody()); // Replace 5 with the expected count
+        assertEquals(9, response.getBody());
     }
 
     @Test
     public void testGetLeaveCount_NotFound() {
-        // Define a sample email
-        String email = "nonexistent@example.com";
-
-        // Mock the service method to throw an exception (simulating a not found scenario)
+        String email = "anu04@gmail.com";
         when(leaveService.getLeaveCountByEmail(email));
-
-        // Call the controller method
         ResponseEntity<Integer> response = leaveController.getLeaveCount(email);
-
-        // Verify that the service method was called
         verify(leaveService, times(1)).getLeaveCountByEmail(email);
-
-        // Assert the response status code (should be NOT_FOUND)
         assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
         assertNull(response.getBody());
     }
     
     @Test
     public void testGetLeavesByEmail() {
-        // Define a sample email
-        String email = "user@example.com";
-
-        // Create a sample list of Leave objects
+        String email = "anunamala04@gmail.com";
         List<Leave> sampleLeaveList = new ArrayList<>();
-        // Add some Leave objects to the list
-
-        // Mock the service method to return the sample list of Leave objects
         when(leaveService.getLeavesByEmail(email)).thenReturn(sampleLeaveList);
-
-        // Call the controller method
         List<Leave> result = leaveController.getLeavesByEmail(email);
-
-        // Verify that the service method was called
         verify(leaveService, times(1)).getLeavesByEmail(email);
-
-        // Assert the result
         assertEquals(sampleLeaveList, result);
     }
 }
